47776fffa5c492e5504e376cb71c6ba0361a9473
CreativeNot <brennerlowe401@gmail.com>
2022-02-04 11:00:45 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        double itemPrice = 15.0;
+        double finalCost = 0;
+
+        finalCost = itemPrice * .05;
+           System.out.println("The price of your item is $" + itemPrice + " the tax is $" + finalCost);
+
+    }
+}
